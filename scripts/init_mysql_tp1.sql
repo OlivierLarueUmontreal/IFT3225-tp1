d1af574ddf7641ea7832bfe7ea98c75acd80a5d1
fauverick <hungnc888@gmail.com>
2026-02-16 16:14:13 -0500

    add exercice to startup script

--- scripts/init_mysql_tp1.sql
+++ scripts/init_mysql_tp1.sql
@@ -5,11 +5,22 @@ CREATE DATABASE IF NOT EXISTS `tp1` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicod
 USE `tp1`;
 
 CREATE TABLE IF NOT EXISTS `accounts` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` TEXT,
   `email` TEXT,
   `password` TEXT,
   `enabled` TINYINT(1) NOT NULL DEFAULT 1,
   `register_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `exercices` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` TEXT,
+  `description` TEXT,
+  `bodyParts` TEXT,
+  `creatorId` INT UNSIGNED,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_exercices_creator` FOREIGN KEY (`creatorId`) REFERENCES `accounts`(`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
